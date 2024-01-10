9baee06fe03adcba89277fa86012d88110851b6a
ZJidong2001 <1436581890@qq.com>
2024-01-10 10:29:36 +0800

    初识C语言

---
+++ 2021_06_07/add.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//int g_val = 2021;  // 未用static修饰全局变量
+
+//static int g_val = 2021;  // static修饰全局变量
+
+//int Add(int x, int y)  // 未用static修饰函数
+//{
+//	return x + y;
+//}
+
+//static int Add(int x, int y)  // static修饰函数
+//{
+//	return x + y;
+//}
